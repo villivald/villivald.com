7c25c8c8c5861deb7806f4b1588d5bde06d07bde
villivald <maxim.villivald@gmail.com>
2022-11-07 21:48:05 +0200

    add intial structure, css reset & custom font

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  return <footer className={styles.footer}></footer>;
+};
+
+export default Footer;
---
+++ components/Grid.tsx
@@ -0,0 +1,19 @@
+import styles from "../styles/Grid.module.css";
+
+const Grid = () => {
+  return (
+    <main className={styles.main}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </main>
+  );
+};
+
+export default Grid;
