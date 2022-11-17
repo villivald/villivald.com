5365784ca834cba7d9a5dd7481cb5bea42aaba68
villivald <maxim.villivald@gmail.com>
2022-11-17 22:21:09 +0200

    add an arrow svg to the language switcher

--- components/LanguageSwitch.tsx
+++ components/LanguageSwitch.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Image from "next/image";
+
+import { useTheme } from "@mui/material/styles";
 import { Switch } from "@mui/material";
 
 import styles from "../styles/Switch.module.css";
@@ -9,6 +12,7 @@ type Props = {
 
 const LanguageSwitch = ({ changeLocale }: Props) => {
   const [checked, setChecked] = useState(false);
+  const theme = useTheme().palette.mode;
 
   const handleChange = () => {
     setChecked(!checked);
@@ -26,6 +30,16 @@ const LanguageSwitch = ({ changeLocale }: Props) => {
       >
         EN 🇺🇸
       </span>
+      <Image
+        className={checked ? styles.arrowRight : styles.arrowLeft}
+        style={{
+          filter: theme === "dark" ? "invert(1)" : "invert(0)",
+        }}
+        src="/images/arrow.svg"
+        alt="Arrow pointing to the chosen language"
+        width={100}
+        height={100}
+      />
       <Switch checked={checked} onChange={handleChange} />
       <span
         className={
