2018cb55937adf48b6f3d26fd3fda5575e1e5c2b
villivald <maxim.villivald@gmail.com>
2022-12-16 18:29:43 +0200

    add main container's background color change on theme swtich

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "./headerComponents/Header";
 import Footer from "./Footer";
+import { useTheme } from "@mui/material/styles";
 
 type Props = {
   children: React.ReactNode;
@@ -8,10 +9,14 @@ type Props = {
 };
 
 export default function Layout({ children, changeTheme, changeLocale }: Props) {
+  const theme = useTheme().palette.mode;
+
   return (
     <>
       <Header changeTheme={changeTheme} changeLocale={changeLocale} />
-      <main>{children}</main>
+      <main style={{ backgroundColor: theme === "dark" ? "#5B5B5B" : "" }}>
+        {children}
+      </main>
       <Footer />
     </>
   );
