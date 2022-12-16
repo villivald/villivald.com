02b386dfe1424fae6de08952148b0ab224ff38c3
villivald <maxim.villivald@gmail.com>
2022-12-16 19:46:52 +0200

    fix issue #3 - create a starting point for uses card, need to investigate further

--- components/gridComponents/UsesGrid.tsx
+++ components/gridComponents/UsesGrid.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 import styles from "../../styles/grid/UsesGrid.module.css";
 
 const UsesGrid = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={styles.container}>
-      <div>
-        <h1>Uses</h1>
-      </div>
+      {!isOpen && <p className={styles.to}>TO</p>}
+      <motion.div
+        layout
+        data-isOpen={isOpen}
+        initial={{ borderRadius: 50 }}
+        className={styles.parent}
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+      >
+        <motion.div layout data-isOpen={isOpen} className={styles.child} />
+      </motion.div>
+      {!isOpen && <p className={styles.ls}>LS</p>}
+
       <div className={styles.gridFooter}>
         <p>✏️Uses</p>
         <p>↗</p>
