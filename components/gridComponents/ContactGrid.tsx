a928f7e31eff13a43786274b9c7a418baabdcb2f
villivald <maxim.villivald@gmail.com>
2023-01-11 21:17:32 +0200

    add phone to contact grid card

--- components/gridComponents/ContactGrid.tsx
+++ components/gridComponents/ContactGrid.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import CardFooter from "./subComponents/CardFooter";
 import styles from "../../styles/grid/ContactGrid.module.css";
 
@@ -5,6 +7,13 @@ const ContactGrid = () => {
   return (
     <div className={styles.container}>
       <div></div>
+      <Image
+        src="/images/phone.webp"
+        className={styles.phone}
+        alt="phone"
+        width={200}
+        height={200}
+      />
       <CardFooter emoji="phone" text="contact" />
     </div>
   );
