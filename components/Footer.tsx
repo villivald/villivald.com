5cc9f31d8affb69000f71c3882f0eeadf912725b
villivald <maxim.villivald@gmail.com>
2023-01-19 22:17:16 +0200

    add initial version of a footer with social icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useTheme } from "@mui/material/styles";
 
 import { colors } from "../utils/colors";
@@ -11,7 +12,27 @@ const Footer = () => {
     <footer
       className={styles.footer}
       style={{ backgroundColor: theme === "dark" ? colors.dark : colors.green }}
-    ></footer>
+    >
+      <div>
+        <div></div>
+      </div>
+      <div>
+        <Image src="/emojis/gitlab.svg" alt="logo" width={64} height={64} />
+        <Image src="/emojis/github.svg" alt="logo" width={64} height={64} />
+        <Image src="/emojis/linkedin.svg" alt="logo" width={64} height={64} />
+      </div>
+      <div>
+        <div></div>
+      </div>
+      <div>
+        <Image src="/emojis/basketball.svg" alt="logo" width={64} height={64} />
+        <Image src="/emojis/email.svg" alt="logo" width={64} height={64} />
+        <Image src="/emojis/pen.svg" alt="logo" width={64} height={64} />
+      </div>
+      <div>
+        <div></div>
+      </div>
+    </footer>
   );
 };
 
