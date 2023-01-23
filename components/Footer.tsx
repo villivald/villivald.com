ca0982a49e58bea2e6f871eb4ca18cdc402dfbec
villivald <maxim.villivald@gmail.com>
2023-01-23 18:35:22 +0200

    fix footer - add links & refactor

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useTheme } from "@mui/material/styles";
 
 import { colors } from "../utils/colors";
@@ -8,30 +9,67 @@ import styles from "../styles/Footer.module.css";
 const Footer = () => {
   const theme = useTheme().palette.mode;
 
+  const socialIcons = [
+    { name: "gitlab", link: "https://version.helsinki.fi/villival" },
+    { name: "github", link: "https://github.com/villivald" },
+    { name: "linkedin", link: "https://www.linkedin.com/in/villivald/" },
+    { name: "mastodon", link: "https://mastodon.world/@villivald" },
+  ];
+
+  const otherIcons = [
+    { name: "basketball", link: "https://www.koripallopaikat.com" },
+    { name: "email", link: "mailto:maxim@villivald.com" },
+    { name: "pen", link: "https://www.create-react-app.com/" },
+    { name: "hackernoon", link: "https://hackernoon.com/u/villivald" },
+  ];
+
+  const Divider = () => (
+    <div>
+      <div
+        style={{
+          backgroundColor: theme === "dark" ? colors.white : colors.dark,
+        }}
+      ></div>
+    </div>
+  );
+
   return (
     <footer
       className={styles.footer}
       style={{ backgroundColor: theme === "dark" ? colors.dark : colors.green }}
     >
+      <Divider />
       <div>
-        <div></div>
+        {socialIcons.map((icon) => (
+          <Link href={icon.link} key={icon.name}>
+            <Image
+              src={`/emojis/${icon.name}.svg`}
+              alt="logo"
+              width={64}
+              height={64}
+            />
+          </Link>
+        ))}
       </div>
+      <Divider />
       <div>
-        <Image src="/emojis/gitlab.svg" alt="logo" width={64} height={64} />
-        <Image src="/emojis/github.svg" alt="logo" width={64} height={64} />
-        <Image src="/emojis/linkedin.svg" alt="logo" width={64} height={64} />
+        {otherIcons.map((icon) => (
+          <Link href={icon.link} key={icon.name}>
+            <Image
+              src={`/emojis/${icon.name}.svg`}
+              alt="logo"
+              width={64}
+              height={icon.name === "hackernoon" ? 48 : 64}
+              style={
+                icon.name === "hackernoon" && theme === "dark"
+                  ? { filter: "invert(1)" }
+                  : {}
+              }
+            />
+          </Link>
+        ))}
       </div>
-      <div>
-        <div></div>
-      </div>
-      <div>
-        <Image src="/emojis/basketball.svg" alt="logo" width={64} height={64} />
-        <Image src="/emojis/email.svg" alt="logo" width={64} height={64} />
-        <Image src="/emojis/pen.svg" alt="logo" width={64} height={64} />
-      </div>
-      <div>
-        <div></div>
-      </div>
+      <Divider />
     </footer>
   );
 };
