f68b28c284742b4eef0c156d1af659e3c7b016a7
villivald <maxim.villivald@gmail.com>
2023-03-14 19:12:36 +0200

    add initial version of contact page

--- components/pageComponents/Contact.tsx
+++ components/pageComponents/Contact.tsx
@@ -1,7 +1,13 @@
 import styles from "../../styles/Contact.module.css";
 
 const Contact = () => {
-  return <div className={styles.mainContainer}></div>;
+  return (
+    <div className={styles.mainContainer}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Contact;
