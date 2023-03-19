03e26d79d3d1d1fde73360895973cd2ea4c4b9fc
villivald <maxim.villivald@gmail.com>
2023-03-19 17:08:16 +0200

    continue implementing uses page

--- components/pageComponents/Uses.tsx
+++ components/pageComponents/Uses.tsx
@@ -3,6 +3,90 @@ import Link from "next/link";
 
 import styles from "../../styles/Uses.module.css";
 
+const hardware = [
+  {
+    name: "iMac",
+    link: "https://www.apple.com/imac-24/",
+    description: "My main computer",
+  },
+  {
+    name: "MacBook Pro",
+    link: "https://www.apple.com/macbook-pro-13/",
+    description: "My secondary computer",
+  },
+  {
+    name: "iPhone 11",
+    link: "https://www.apple.com/iphone-11/",
+    description: "My main phone",
+  },
+  {
+    name: "iPad",
+    link: "https://www.apple.com/ipad/",
+    description: "My main tablet",
+  },
+  {
+    name: "Apple Watch Ultra",
+    link: "https://www.apple.com/apple-watch/",
+    description: "My main watch",
+  },
+  {
+    name: "Marshall Majot IV",
+    link: "https://www.marshallheadphones.com/major-iv",
+    description: "My main headphones",
+  },
+  {
+    name: "Logitech MX Master 3",
+    link: "https://www.logitech.com/en-us/product/mx-master-3",
+    description: "My main mouse",
+  },
+  {
+    name: "Keychron K2",
+    link: "https://www.keychron.com/products/keychron-k2-wireless-mechanical-keyboard",
+    description: "My main keyboard",
+  },
+  {
+    name: "Magic Trackpad",
+    link: "https://www.apple.com/magic-trackpad-2/",
+    description: "My main trackpad",
+  },
+];
+
+const software = [
+  {
+    name: "Visual Studio Code",
+    link: "https://code.visualstudio.com/",
+    description: "My main code editor",
+  },
+  {
+    name: "Figma",
+    link: "https://www.figma.com/",
+    description: "My main design tool",
+  },
+  {
+    name: "Notion",
+    link: "https://www.notion.so/",
+    description: "My main note taking app",
+  },
+];
+
+const other = [
+  {
+    name: "Apple AirPods Pro",
+    link: "https://www.apple.com/airpods-pro/",
+    description: "My main earphones",
+  },
+  {
+    name: "Apple AirPods Max",
+    link: "https://www.apple.com/airpods-max/",
+    description: "My main headphones",
+  },
+  {
+    name: "Apple Magic Keyboard",
+    link: "https://www.apple.com/magic-keyboard/",
+    description: "My main keyboard",
+  },
+];
+
 const Uses = () => {
   return (
     <div className={styles.mainContainer}>
@@ -36,45 +120,39 @@ const Uses = () => {
         </map>
       </figure>
       <p>
-        The following is a list of the tools and software that I use on a daily
-        basis. This page was inspired by Wes Bos and his project -{" "}
-        <Link href="https://uses.tech/">uses.tech</Link>.
+        The following is a list of the tools, software and any other things that
+        I use on a daily basis. This page was inspired by Wes Bos and his
+        project - <Link href="https://uses.tech/">uses.tech</Link>.
       </p>
       <div>
         <div>
           <h1>Hardware</h1>
           <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+            {hardware.map((item) => (
+              <li key={item.name}>
+                <Link href={item.link}>{item.name}</Link>
+              </li>
+            ))}
           </ul>
         </div>
         <div>
           <h1>Software</h1>
           <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+            {software.map((item) => (
+              <li key={item.name}>
+                <Link href={item.link}>{item.name}</Link>
+              </li>
+            ))}
           </ul>
         </div>
         <div>
           <h1>Other</h1>
           <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+            {other.map((item) => (
+              <li key={item.name}>
+                <Link href={item.link}>{item.name}</Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
