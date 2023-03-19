91ca1c19eafba1a38cf7fd48ff927dd175ed577a
villivald <maxim.villivald@gmail.com>
2023-03-19 17:52:39 +0200

    continue fixing uses, old, & contact pages - version 0.2.4 :wrench

--- components/pageComponents/Contact.tsx
+++ components/pageComponents/Contact.tsx
@@ -1,11 +1,29 @@
+import Image from "next/image";
+
 import styles from "../../styles/Contact.module.css";
 
 const Contact = () => {
   return (
     <div className={styles.mainContainer}>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <Image
+          src="/emojis/mastodon.svg"
+          width={500}
+          height={500}
+          alt="Contact"
+        />
+      </div>
+      <div>
+        <Image src="/emojis/email.svg" width={500} height={500} alt="Contact" />
+      </div>
+      <div>
+        <Image
+          src="/emojis/linkedin.svg"
+          width={500}
+          height={500}
+          alt="Contact"
+        />
+      </div>
     </div>
   );
 };
