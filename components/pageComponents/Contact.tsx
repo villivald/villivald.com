8041e9b24e88b3a43353ee08c03997f8fc746424
villivald <maxim.villivald@gmail.com>
2023-03-23 18:10:10 +0200

    add links to the contact page - version 0.2.6 :wrench

--- components/pageComponents/Contact.tsx
+++ components/pageComponents/Contact.tsx
@@ -1,28 +1,43 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useIntl } from "react-intl";
 
 import styles from "../../styles/Contact.module.css";
 
 const Contact = () => {
+  const intl = useIntl();
+
   return (
     <div className={styles.mainContainer}>
       <div>
-        <Image
-          src="/emojis/mastodon.svg"
-          width={500}
-          height={500}
-          alt="Contact"
-        />
+        <Link href="https://mastodon.world/@villivald" target="_blank">
+          <Image
+            src="/emojis/mastodon.svg"
+            width={500}
+            height={500}
+            alt={intl.formatMessage({ id: "contact.link.mastodon" })}
+          />
+        </Link>
       </div>
       <div>
-        <Image src="/emojis/email.svg" width={500} height={500} alt="Contact" />
+        <Link href="mailto:maxim@villivald.com" target="_blank">
+          <Image
+            src="/emojis/email.svg"
+            width={500}
+            height={500}
+            alt={intl.formatMessage({ id: "contact.link.mail" })}
+          />
+        </Link>
       </div>
       <div>
-        <Image
-          src="/emojis/linkedin.svg"
-          width={500}
-          height={500}
-          alt="Contact"
-        />
+        <Link href="https://www.linkedin.com/in/villivald/" target="_blank">
+          <Image
+            src="/emojis/linkedin.svg"
+            width={500}
+            height={500}
+            alt={intl.formatMessage({ id: "contact.link.linkedin" })}
+          />
+        </Link>
       </div>
     </div>
   );
