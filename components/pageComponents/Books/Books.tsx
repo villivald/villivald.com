3fef81c5bfd954383a2a14db0206ba6a7143a3f1
villivald <maxim.villivald@gmail.com>
2023-04-07 18:07:20 +0300

    fix books data json import

--- components/pageComponents/Books/Books.tsx
+++ components/pageComponents/Books/Books.tsx
@@ -2,12 +2,12 @@ import { useMemo } from "react";
 import Image from "next/image";
 import { VirtuosoGrid } from "react-virtuoso";
 
-import { books } from "./Data.json";
+import data from "./Data.json";
 
 import styles from "../../../styles/Books.module.css";
 
 const Books = () => {
-  const sortedBooks = useMemo(() => [...books].reverse(), []);
+  const sortedBooks = useMemo(() => [...data.books].reverse(), []);
 
   return (
     <div>
