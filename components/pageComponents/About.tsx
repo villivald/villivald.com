42f216ffc97827e1c49f65cd0327f4fa288468a0
villivald <maxim.villivald@gmail.com>
2023-05-01 19:50:25 +0300

    add initial version of about page content - carousel

--- components/pageComponents/About.tsx
+++ components/pageComponents/About.tsx
@@ -43,6 +43,93 @@ const About = () => {
           </div>
         </div>
       </div>
+
+      <section className={styles.carousel} aria-label="carousel" tabIndex={0}>
+        <div className={styles.slides}>
+          <div
+            className={styles.slidesItem}
+            id="slide-1"
+            data-slide="slide1"
+            aria-label="slide 1 of 5"
+            tabIndex={0}
+          >
+            1
+          </div>
+          <div
+            className={styles.slidesItem}
+            id="slide-2"
+            data-slide="slide2"
+            aria-label="slide 2 of 5"
+            tabIndex={0}
+          >
+            2
+          </div>
+          <div
+            className={styles.slidesItem}
+            id="slide-3"
+            data-slide="slide3"
+            aria-label="slide 3 of 5"
+            tabIndex={0}
+          >
+            3
+          </div>
+          <div
+            className={styles.slidesItem}
+            id="slide-4"
+            data-slide="slide4"
+            aria-label="slide 4 of 5"
+            tabIndex={0}
+          >
+            4
+          </div>
+          <div
+            className={styles.slidesItem}
+            id="slide-5"
+            data-slide="slide5"
+            aria-label="slide 5 of 5"
+            tabIndex={0}
+          >
+            5
+          </div>
+        </div>
+        <div className={styles.carousel__nav}>
+          <a
+            className={styles.sliderNav}
+            href="#slide-1"
+            aria-label="Go to slide 1"
+          >
+            1
+          </a>
+          <a
+            className={styles.sliderNav}
+            href="#slide-2"
+            aria-label="Go to slide 2"
+          >
+            2
+          </a>
+          <a
+            className={styles.sliderNav}
+            href="#slide-3"
+            aria-label="Go to slide 3"
+          >
+            3
+          </a>
+          <a
+            className={styles.sliderNav}
+            href="#slide-4"
+            aria-label="Go to slide 4"
+          >
+            4
+          </a>
+          <a
+            className={styles.sliderNav}
+            href="#slide-5"
+            aria-label="Go to slide 5"
+          >
+            5
+          </a>
+        </div>
+      </section>
     </div>
   );
 };
