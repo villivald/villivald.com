211d7abb5ad2b52bec4dc6e12d56458bbe744343
villivald <maxim.villivald@gmail.com>
2023-05-05 22:10:45 +0300

    continue implementing about page

--- components/pageComponents/About.tsx
+++ components/pageComponents/About.tsx
@@ -1,6 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 import styles from "../../styles/About.module.css";
 
 const About = () => {
+  const { asPath } = useRouter();
+  const [currentSlide, setCurrentSlide] = useState("2023");
+
+  const years = ["2023", "2022", "2021", "2020", "2019"];
+
+  useEffect(() => {
+    const slide1 = document.getElementById("2023");
+    const slide2 = document.getElementById("2022");
+    const slide3 = document.getElementById("2021");
+    const slide4 = document.getElementById("2020");
+    const slide5 = document.getElementById("2019");
+
+    const slides = [slide1, slide2, slide3, slide4, slide5];
+
+    slides.forEach((slide) => {
+      // TODO: investigate is there better way to do this than "any"
+      slide?.addEventListener("click", (e: any) => {
+        setCurrentSlide(e.currentTarget?.id);
+      });
+    });
+  }, [asPath]);
+
   const handleEyeMove = (e: React.PointerEvent<HTMLDivElement>) => {
     // get the x and y coordinates of the pointer
     const { clientX, clientY } = e;
@@ -53,7 +78,7 @@ const About = () => {
             aria-label="slide 1 of 5"
             tabIndex={0}
           >
-            1
+            2023
           </div>
           <div
             className={styles.slidesItem}
@@ -62,7 +87,7 @@ const About = () => {
             aria-label="slide 2 of 5"
             tabIndex={0}
           >
-            2
+            2022
           </div>
           <div
             className={styles.slidesItem}
@@ -71,7 +96,7 @@ const About = () => {
             aria-label="slide 3 of 5"
             tabIndex={0}
           >
-            3
+            2021
           </div>
           <div
             className={styles.slidesItem}
@@ -80,7 +105,7 @@ const About = () => {
             aria-label="slide 4 of 5"
             tabIndex={0}
           >
-            4
+            2020
           </div>
           <div
             className={styles.slidesItem}
@@ -89,45 +114,22 @@ const About = () => {
             aria-label="slide 5 of 5"
             tabIndex={0}
           >
-            5
+            2019
           </div>
         </div>
         <div className={styles.carousel__nav}>
-          <a
-            className={styles.sliderNav}
-            href="#slide-1"
-            aria-label="Go to slide 1"
-          >
-            1
-          </a>
-          <a
-            className={styles.sliderNav}
-            href="#slide-2"
-            aria-label="Go to slide 2"
-          >
-            2
-          </a>
-          <a
-            className={styles.sliderNav}
-            href="#slide-3"
-            aria-label="Go to slide 3"
-          >
-            3
-          </a>
-          <a
-            className={styles.sliderNav}
-            href="#slide-4"
-            aria-label="Go to slide 4"
-          >
-            4
-          </a>
-          <a
-            className={styles.sliderNav}
-            href="#slide-5"
-            aria-label="Go to slide 5"
-          >
-            5
-          </a>
+          {years.map((slideLink, index) => (
+            <a
+              key={index}
+              id={slideLink}
+              className={styles.sliderNav}
+              href={`#slide-${++index}`}
+              aria-label={`Go to slide ${++index}`}
+              data-slide={currentSlide === slideLink ? "active" : ""}
+            >
+              {slideLink}
+            </a>
+          ))}
         </div>
       </section>
     </div>
