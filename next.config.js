16e35484e413a0b64d45162538640fba5e9a93fd
villivald <maxim.villivald@gmail.com>
2024-04-14 17:27:35 +0300

    remove unnecessary mdx packages & config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    providerImportSource: "@mdx-js/react",
-  },
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -35,7 +26,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withMDX({
-  ...nextConfig,
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-});
+module.exports = nextConfig;
