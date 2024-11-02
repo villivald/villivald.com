76ce7c203e74db6e3f41abc1448455011451be3f
villivald <maxim.villivald@gmail.com>
2024-11-02 19:10:24 +0200

    add a closed eye effect to the now grid card

--- components/gridComponents/NowGrid.tsx
+++ components/gridComponents/NowGrid.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import CardFooter from "./subComponents/CardFooter";
 
 import styles from "../../styles/grid/NowGrid.module.css";
 
 export default function NowGrid() {
+  const [eye, setEye] = useState<string>("open");
+
   return (
-    <div className={styles.container} aria-hidden="true">
-      <div className={styles.now} />
+    <div
+      className={styles.container}
+      aria-hidden="true"
+      onMouseEnter={() => setEye("closed")}
+      onMouseLeave={() => setEye("open")}
+    >
+      <div className={styles.now} data-eye={eye} />
       <CardFooter emoji="now" text="now" />
     </div>
   );
