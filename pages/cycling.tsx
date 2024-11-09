c3113ccbad1db7f16a3248b35234d67cb67baaea
villivald <maxim.villivald@gmail.com>
2024-11-09 20:15:52 +0200

    fix deployment - #55

--- pages/cycling.tsx
+++ pages/cycling.tsx
@@ -1,30 +1,28 @@
 import { useEffect, useState } from "react";
 
 export default function Cycling() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<{ activity_date: string }[]>([]);
 
   useEffect(() => {
     const fetchActivities = async () => {
       const response = await fetch("/api/strava/activities");
       const data = await response.json();
-
-      // Filter activities for October 2024
-      const filteredActivities = data.filter((activity) => {
-        const activityDate = new Date(activity.activity_date); // Assuming the date is stored in activity.activity_date
-        const activityMonth = activityDate.getMonth(); // 0-based (0 = January, 1 = February, etc.)
-        const activityYear = activityDate.getFullYear();
-
-        // Check if the activity is in October 2024
-        return activityMonth === 10 && activityYear === 2024; // 9 represents October (0-based)
-      });
-
-      setActivities(filteredActivities);
+      setActivities(data);
     };
 
     fetchActivities();
   }, []);
 
-  console.log(activities); // Log filtered activities
+  const filteredActivities: any[] = activities.filter((activity) => {
+    const activityDate = new Date(activity.activity_date); // Assuming the date is stored in activity.activity_date
+    const activityMonth = activityDate.getMonth(); // 0-based (0 = January, 1 = February, etc.)
+    const activityYear = activityDate.getFullYear();
+
+    // Check if the activity is in October 2024
+    return activityMonth === 9 && activityYear === 2024; // 9 represents October (0-based)
+  });
+
+  console.log(filteredActivities);
 
   return <div />;
 }
