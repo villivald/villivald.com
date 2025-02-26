e58baee038f482791a34ce7b9e1c27a4afd5b400
villivald <maxim.villivald@gmail.com>
2025-02-26 20:25:19 +0200

    make3 last years visible by default on books statistics page

--- components/pageComponents/Books/BarChart.tsx
+++ components/pageComponents/Books/BarChart.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -12,7 +12,6 @@ import { Bar } from "react-chartjs-2";
 import { useIntl } from "react-intl";
 
 import data from "./data.json";
-
 import { ThemeContext } from "../../../context";
 
 ChartJS.register(
@@ -33,9 +32,9 @@ const options = {
   },
 };
 
-const monthLabels = Array.from({ length: 12 }, (_, month) => {
-  return new Date(0, month).toLocaleString("en-US", { month: "long" });
-});
+const monthLabels = Array.from({ length: 12 }, (_, month) =>
+  new Date(0, month).toLocaleString("en-US", { month: "long" }),
+);
 
 const handleData = (year: string) => {
   return data.books
@@ -49,21 +48,28 @@ const handleData = (year: string) => {
 };
 
 const booksDataSets = [
-  { "2024": "#050909" },
+  { "2025": "#f58231" },
+  { "2024": "#bc5090" },
   { "2023": "#003f5c" },
   { "2022": "#58508d" },
-  { "2021": "#bc5090" },
+  { "2021": "#050909" },
   { "2020": "#ff6361" },
   { "2019": "#ffa600" },
 ];
 
+// Get the last two years dynamically
+const availableYears = booksDataSets.map((year) => Object.keys(year)[0]);
+const lastThreeYears = availableYears.slice(0, 3);
+
 const books = {
   labels: monthLabels,
   datasets: booksDataSets.map((year) => {
+    const yearLabel = Object.keys(year)[0];
     return {
-      label: Object.keys(year)[0],
-      data: handleData(Object.keys(year)[0]),
+      label: yearLabel,
+      data: handleData(yearLabel),
       backgroundColor: Object.values(year)[0],
+      hidden: !lastThreeYears.includes(yearLabel),
     };
   }),
 };
@@ -72,11 +78,9 @@ export default function BarChart() {
   const theme = useContext(ThemeContext);
   const intl = useIntl();
 
-  if (theme === "dark") {
-    ChartJS.defaults.color = "#fff";
-  } else {
-    ChartJS.defaults.color = "#1c1d2b";
-  }
+  useEffect(() => {
+    ChartJS.defaults.color = theme === "dark" ? "#fff" : "#1c1d2b";
+  }, [theme]);
 
   return (
     <Bar
