17f34341f2eb239a2edf38e31565fa541a2a6647
Maxim Villivald <maxim@villivald.com>
2025-05-16 23:14:41 +0300

    update books page

--- components/pageComponents/Books/Books.tsx
+++ components/pageComponents/Books/Books.tsx
@@ -67,7 +67,7 @@ export default function Books() {
           height={32}
         />
         <FormattedMessage id="lastUpdated" />
-        <time dateTime="2025-04-20">20.04.2025</time>
+        <time dateTime="2025-05-16">16.05.2025</time>
       </p>
 
       <div className={styles.booksContainer} ref={parentRef}>
