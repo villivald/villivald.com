9559b5c6745268a0613d957c53d506de4d41a57e
Maxim Villivald <maxim.villivald@gmail.com>
2025-11-12 22:58:53 +0200

    update books page

--- components/pageComponents/Books/Books.tsx
+++ components/pageComponents/Books/Books.tsx
@@ -132,7 +132,7 @@ export default function Books() {
           height={32}
         />
         <FormattedMessage id="lastUpdated" />
-        <time dateTime="2025-10-14">14.10.2025</time>
+        <time dateTime="2025-11-12">12.11.2025</time>
       </p>
 
       {/* Filters */}
